bde91222a8b684c161a235a77545aac110a805a9
Dmitry D <vantive2000@gmail.com>
2018-07-23 23:49:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = string
   var lowercase = string
+<<<<<<< HEAD
   var iLoveYou = "I love you, Grandma."
   
 
-  if (string === iLoveYou)
-    {return ("I love you, too.")}
+
     
   if 
     (lowercase.toLowerCase() === lowercase)
@@ -28,5 +28,21 @@ function sayHiToGrandma(string) {
   else
     (uppercase.toUpperCase() === uppercase)
     {return ("YES INDEED!")    }
+    
+  if (string === iLoveYou)
+    {return ("I love you, too.")}
+    
 
+=======
+  var iLoveYou = string
+  
+  
+  if (lowercase.toLowerCase() === lowercase)
+    {return (`I can't hear you!`)}
+  else
+    (uppercase.toUpperCase() === uppercase)
+    {return ("YES INDEED!")
+    }
+  
+>>>>>>> 52281ec2f6ea7a2a620594b736bf22b72247205a
 }
